0e11b58b873d1b035222f71489491bec5662b530
ganesan0726 <ganeshrajendran79@gmail.com>
2023-11-01 22:02:52 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -9,17 +9,28 @@ import Missing from "./Missing";
 import Footer from "./Footer";
 import { format } from "date-fns";
 import { Route, Routes, useNavigate } from "react-router-dom";
-
-import api from "./api/post";
+import api from "./api/axios";
+import EditPost from "./EditPost";
+import useWindowSize from "./CustomHooks/useWindowSize";
+import useAxiosFetch from "./CustomHooks/useAxiosFetch";
 
 const App = () => {
   const [posts, setPosts] = useState([]);
-
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [postTitle, setPostTitle] = useState("");
   const [postBody, setPostBody] = useState("");
+  const [editTitle, setEditTitle] = useState("");
+  const [editBody, setEditBody] = useState("");
   const navigate = useNavigate();
+  const { width } = useWindowSize();
+  const { data, fetchError, isLoading } = useAxiosFetch(
+    "http://localhost:3500/posts",
+  );
+
+  useEffect(() => {
+    setPosts(data);
+  }, [data]);
 
   useEffect(() => {
     const filteredResults = posts.filter(
@@ -30,30 +41,87 @@ const App = () => {
     setSearchResults(filteredResults.reverse());
   }, [posts, search]);
 
+  // Add Data into the server = Post operation :
   const handleSubmit = async (e) => {
     e.preventDefault();
     const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
     const datetime = format(new Date(), "MMMM dd, yyyy pp");
     const newPost = { id, title: postTitle, datetime, body: postBody };
-    const allPosts = [...posts, newPost];
-    setPosts(allPosts);
-    setPostTitle("");
-    setPostBody("");
-    navigate("/");
+    try {
+      const response = await api.post("/posts", newPost);
+      const allPosts = [...posts, response.data];
+      setPosts(allPosts);
+      setPostTitle("");
+      setPostBody("");
+      navigate("/");
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else {
+        console.log(`Error : ${error.message}`);
+      }
+    }
   };
 
-  const handleDelete = (id) =>{
-      const postList = posts.filter(post => post.id !== id);
+  // Edit / Update the data into the server using Patch/ Put operation :
+
+  const handleEdit = async (id) => {
+    const datetime = format(new Date(), "MMMM dd, yyyy pp");
+    const updatedPost = { id, title: editTitle, datetime, body: editBody };
+    try {
+      const response = await api.put(`/posts/${id}`, updatedPost);
+      setPosts(
+        posts.map((post) => (post.id === id ? { ...response.data } : post)),
+      );
+      setEditTitle("");
+      setEditBody("");
+      navigate("/");
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else {
+        console.log(`Error : ${error.message}`);
+      }
+    }
+  };
+
+  //  Delete the Data into the server using Delete Operation :
+  const handleDelete = async (id) => {
+    try {
+      await api.delete(`/posts/${id}`);
+      const postList = posts.filter((post) => post.id !== id);
       setPosts(postList);
       navigate("/");
-  }
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else {
+        console.log(`Error : ${error.message}`);
+      }
+    }
+  };
 
   return (
     <div className="App">
-      <Header title="Social Media App" />
+      <Header title="Social Media App" width={width} />
       <Nav search={search} setSearch={setSearch} />
       <Routes>
-        <Route path="/" element={<Home posts={searchResults} />} />
+        <Route
+          path="/"
+          element={
+            <Home
+              posts={searchResults}
+              fetchError={fetchError}
+              isLoading={isLoading}
+            />
+          }
+        />
         <Route path="post">
           <Route
             index
@@ -72,7 +140,19 @@ const App = () => {
             element={<PostPage posts={posts} handleDelete={handleDelete} />}
           />
         </Route>
-
+        <Route
+          path="/edit/:id"
+          element={
+            <EditPost
+              posts={posts}
+              handleEdit={handleEdit}
+              editBody={editBody}
+              setEditBody={setEditBody}
+              editTitle={editTitle}
+              setEditTitle={setEditTitle}
+            />
+          }
+        />
         <Route path="about" element={<About />} />
         <Route path="*" element={<Missing />} />
       </Routes>
@@ -82,3 +162,22 @@ const App = () => {
 };
 
 export default App;
+
+// // Fetch the Data from server using Get operation :
+// useEffect(() => {
+//   const fetchData = async () => {
+//     try {
+//       const response = await api.get("/posts");
+//       setPosts(response.data);
+//     } catch (error) {
+//       if (error.response) {
+//         console.log(error.response.data);
+//         console.log(error.response.status);
+//         console.log(error.response.headers);
+//       } else {
+//         console.log(`Error : ${error.message}`);
+//       }
+//     }
+//   };
+//   fetchData();
+// }, []);
